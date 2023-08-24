509aa605262cd800e88c7372e1d3d19f58be47f7
dragonalias <dragon1600@gmail.com>
2023-08-24 23:25:03 +0200

    Added way to open Youtube Subscription page

--- background.js
+++ background.js
@@ -23,6 +23,14 @@ browser.commands.onCommand.addListener(function (command) {
       duplicating.then(onDuplicated, onError);
     });
   }
+
+  if (command === "open_yt_sub")
+  {
+    browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
+      let index = (tabs[0].index + 1);
+      browser.tabs.create({index, url: "https://www.youtube.com/feed/subscriptions"});
+    });
+  }
 });
 
 
